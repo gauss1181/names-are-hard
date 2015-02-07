608b38dc0863e543377bdf9961632b5c6bf89b7e
Vincent Huang <vincom2@gmail.com>
2015-02-07 14:36:50 -0500

    use $PORT

--- hello.py
+++ hello.py
@@ -55,4 +55,5 @@ def search_data():
     return json.dumps([(s[0],str(s[1])) for s in schedules[:5]])
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, port=port)
