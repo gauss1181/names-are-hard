3be3c54a63983aff0813b4e8e2420e232965d364
Vincent Huang <vincom2@gmail.com>
2015-02-07 00:38:29 -0500

    MONGODB IS WEB SCALE

--- enter_course.py
+++ enter_course.py
@@ -1,10 +1,16 @@
 from flask import Flask
-from flask.ext.pymongo import PyMongo
 from flask import request
 from flask import render_template
 
+#from flask.ext.pymongo import PyMongo
+import pymongo
+from pymongo import MongoClient
+
 app = Flask(__name__)
-mongo = PyMongo(app)
+mongo = MongoClient()
+
+db = mongo.course_database
+courses = db.courses
 
 @app.route('/')
 def my_form():
@@ -15,8 +21,9 @@ def my_form_post():
     course_number = request.form['course_number']
     course_name = request.form['course_name']
     processed_text = course_number + ": " + course_name
+    course_id = courses.insert({"name" : course_name, "number" : course_number})
 
-    return processed_text
+    return course_id
 
 if __name__ == '__main__':
     app.run()
