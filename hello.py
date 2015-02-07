5b4846a133ac301153357ec9603e353a078664f5
Joel Choo <joelchoo@gmail.com>
2015-02-07 15:00:33 -0500

    added host

--- hello.py
+++ hello.py
@@ -58,4 +58,4 @@ def search_data():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, port=port)
+    app.run(debug=True, host='0.0.0.0', port=port)
